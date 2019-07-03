34064baef07bbfe27df931b46dcd841a9b9f59d9
Adam Chernick <ajc@shoparc.com>
2019-07-03 11:34:04 -0400

    Check Which browser
    Check Which browser

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    // do things here
+    //  check which browser the user is on.. Then curate the page.
+    var browserString = navigator.sayswho;
+    console.log(browserString);
+
+    // if iOS
+    if(browserString){
+
+        $('#ios_quickLook').removeClass('hide');
+        $('#ios_quickLook').addClass('show');
+    
+        $('#android_quickLook').removeClass('show');
+        $('#android_quickLook').addClass('hide');
+    }
+    // else if(){
+
+    // }
 
 });
+
+// find what browser the user is using
+navigator.sayswho= (function(){
+    var N= navigator.appName, ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*([\d\.]+)/i);
+    if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
+    M= M? [M[1], M[2]]:[N, navigator.appVersion, '-?'];
+    return M.join(' ');
+})();
